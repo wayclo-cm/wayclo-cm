b1aca852deccc5f26215d38b9cd1c2ab54b3b8af
wayclo-cm <141838889+wayclo-cm@users.noreply.github.com>
2024-06-27 11:45:20 -0300

    Update Verify-ESXiAdvancedSettings.ps1

--- Verify-ESXiAdvancedSettings.ps1
+++ Verify-ESXiAdvancedSettings.ps1
@@ -15,6 +15,33 @@ Connect-VIServer -Server $vCenterServer -Credential $credential
 $settings = @{
     "Config.HostAgent.plugins.hostsvc.esxAdminsGroupAutoAdd" = $false;
     "Config.HostAgent.plugins.vimsvc.authValidateInterval" = 90;
+    "Security.AccountUnlockTime" = 900;
+    "Security.AccountLockFailures" = 5;
+    "Security.PasswordHistory" = 5;
+    "Syslog.global.logDir" = "Site Specific";
+    "Syslog.global.logHost" = "Site Specific";
+    "Net.BlockGuestBPDU" = 1;
+    "UserVars.ESXiShellInteractiveTimeOut" = 900;
+    "UserVars.ESXiShellTimeOut" = 600;
+    "Security.PasswordQualityControl" = "retry=3 min=disabled,15,15,15,15 max=64 similar=deny passphrase=3";
+    "UserVars.SuppressHyperthreadWarning" = 0;
+    "UserVars.DcuiTimeOut" = 600;
+    "Config.HostAgent.plugins.solo.enableMob" = $false;
+    "DCUI.Access" = "root";
+    "Config.HostAgent.log.level" = "info";
+    "Net.DVFilterBindIpAddress" = "N/A";
+    "UserVars.SuppressShellWarning" = 0;
+    "UserVars.ESXiVPsDisabledProtocols" = "sslv3,tlsv1,tlsv1.1";
+    "Mem.ShareForceSalting" = 2;
+    "Syslog.global.auditRecord.storageEnable" = $true;
+    "Syslog.global.auditRecord.storageCapacity" = 100;
+    "Syslog.global.auditRecord.storageDirectory" = "Site Specific";
+    "Syslog.global.auditRecord.remoteEnable" = $true;
+    "Syslog.global.logLevel" = "info";
+    "Syslog.global.certificate.strictX509Compliance" = $true;
+    "Mem.MemEagerZero" = 1;
+    "Net.BMCNetworkEnable" = 0;
+    "ConfigManager.HostAccessManager.LockdownMode" = "normal";
 }
 
 # Obtener todos los hosts ESXi
@@ -127,7 +154,7 @@ $html += @"
 "@
 
 # Guardar el contenido HTML en un archivo
-$outputFilePath = "$outputFileName.html"
+$outputFilePath = "C:\Scripts\$outputFileName.html"
 $html | Out-File -FilePath $outputFilePath -Encoding UTF8
 
 # Desconectar de vCenter
