f49f8455050e7fa64484f1426f7231d7fe677161
wayclo-cm <141838889+wayclo-cm@users.noreply.github.com>
2024-06-27 12:34:32 -0300

    Update Hardening_vSphere.ps1

--- Hardening_vSphere.ps1
+++ Hardening_vSphere.ps1
@@ -13,35 +13,35 @@ Connect-VIServer -Server $vCenterServer -Credential $credential
 
 # Configuraciones avanzadas a verificar
 $settings = @{
-    "Config.HostAgent.plugins.hostsvc.esxAdminsGroupAutoAdd" = $false
-    "Config.HostAgent.plugins.vimsvc.authValidateInterval" = 90
-    "Security.AccountUnlockTime" = 900
-    "Security.AccountLockFailures" = 5
-    "Security.PasswordHistory" = 5
-    "Syslog.global.logDir" = "Site Specific"
-    "Syslog.global.logHost" = "Site Specific"
-    "Net.BlockGuestBPDU" = 1
-    "UserVars.ESXiShellInteractiveTimeOut" = 900
-    "UserVars.ESXiShellTimeOut" = 600
-    "Security.PasswordQualityControl" = "retry=3 min=disabled,15,15,15,15 max=64 similar=deny passphrase=3"
-    "UserVars.SuppressHyperthreadWarning" = 0
-    "UserVars.DcuiTimeOut" = 600
-    "Config.HostAgent.plugins.solo.enableMob" = $false
-    "DCUI.Access" = "root"
-    "Config.HostAgent.log.level" = "info"
-    "Net.DVFilterBindIpAddress" = "N/A"
-    "UserVars.SuppressShellWarning" = 0
-    "UserVars.ESXiVPsDisabledProtocols" = "sslv3,tlsv1,tlsv1.1"
-    "Mem.ShareForceSalting" = 2
-    "Syslog.global.auditRecord.storageEnable" = $true
-    "Syslog.global.auditRecord.storageCapacity" = 100
-    "Syslog.global.auditRecord.storageDirectory" = "Site Specific"
-    "Syslog.global.auditRecord.remoteEnable" = $true
-    "Syslog.global.logLevel" = "info"
-    "Syslog.global.certificate.strictX509Compliance" = $true
-    "Mem.MemEagerZero" = 1
-    "Net.BMCNetworkEnable" = 0
-    "ConfigManager.HostAccessManager.LockdownMode" = "normal"
+    "Config.HostAgent.plugins.hostsvc.esxAdminsGroupAutoAdd" = $false;
+    "Config.HostAgent.plugins.vimsvc.authValidateInterval" = 90;
+    "Security.AccountUnlockTime" = 900;
+    "Security.AccountLockFailures" = 5;
+    "Security.PasswordHistory" = 5;
+    "Syslog.global.logDir" = "Site Specific";
+    "Syslog.global.logHost" = "Site Specific";
+    "Net.BlockGuestBPDU" = 1;
+    "UserVars.ESXiShellInteractiveTimeOut" = 900;
+    "UserVars.ESXiShellTimeOut" = 600;
+    "Security.PasswordQualityControl" = "retry=3 min=disabled,15,15,15,15 max=64 similar=deny passphrase=3";
+    "UserVars.SuppressHyperthreadWarning" = 0;
+    "UserVars.DcuiTimeOut" = 600;
+    "Config.HostAgent.plugins.solo.enableMob" = $false;
+    "DCUI.Access" = "root";
+    "Config.HostAgent.log.level" = "info";
+    "Net.DVFilterBindIpAddress" = "N/A";
+    "UserVars.SuppressShellWarning" = 0;
+    "UserVars.ESXiVPsDisabledProtocols" = "sslv3,tlsv1,tlsv1.1";
+    "Mem.ShareForceSalting" = 2;
+    "Syslog.global.auditRecord.storageEnable" = $true;
+    "Syslog.global.auditRecord.storageCapacity" = 100;
+    "Syslog.global.auditRecord.storageDirectory" = "Site Specific";
+    "Syslog.global.auditRecord.remoteEnable" = $true;
+    "Syslog.global.logLevel" = "info";
+    "Syslog.global.certificate.strictX509Compliance" = $true;
+    "Mem.MemEagerZero" = 1;
+    "Net.BMCNetworkEnable" = 0;
+    "ConfigManager.HostAccessManager.LockdownMode" = "normal";
 }
 
 # Obtener todos los hosts ESXi
@@ -158,7 +158,7 @@ foreach ($host in $resultadosPorHost.Keys) {
 $html += @"
 </body>
 </html>
-"
+"@
 
 # Guardar el contenido HTML en un archivo
 $outputFilePath = "C:\Scripts\$outputFileName.html"
